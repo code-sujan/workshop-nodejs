1b85b0a108a254cf5d0708ea2ff448aa68523005
code-sujan <asmitment@gmail.com>
2023-06-02 10:42:47 +0545

    add layouting

--- app.js
+++ app.js
@@ -2,13 +2,16 @@ require('module-alias/register');
 const express = require('express');
 const path = require('path');
 const routes = require('./routes/index');
+const ejsLayouts = require('express-ejs-layouts');
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, "src/views"));
 app.use(express.json());
 app.use(express.urlencoded({extended : false}));
-app.use(express.static(path.join(__dirname, "public")))
+app.use(express.static(path.join(__dirname, "/public")));
+app.use(ejsLayouts);
+app.set("layout extractScripts", true);
 
 app.use('/', routes);
 
